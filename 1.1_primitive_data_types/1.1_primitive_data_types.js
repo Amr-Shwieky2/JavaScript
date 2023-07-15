9877eb370cc4cccc21335a37857f99f4ecf64ce9
Amr-Shwieky2 <131922418+Amr-Shwieky2@users.noreply.github.com>
2023-07-15 20:44:00 +0300

    1.1_primitive_data_types

---
+++ 1.1_primitive_data_types/1.1_primitive_data_types.js
@@ -0,0 +1,17 @@
+let message = "You enter a dimly lit room";
+let treasureCount = 4;
+let key = { type: "golden", carvings: "intricate" };
+let TrapActivated = true;
+let secretPassage = null;
+let mysteriousArtifact;
+let ancientSymbol = Symbol("ancient");
+
+
+
+console.log(typeof message);
+console.log(typeof treasureCount);
+console.log(typeof key);
+console.log(typeof TrapActivated);
+console.log(typeof secretPassage);
+console.log(typeof message);
+console.log(typeof message);
