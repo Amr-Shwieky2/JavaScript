4f6adc0d987ed904a98484e3a571227221100604
Amr-Shwieky2 <131922418+Amr-Shwieky2@users.noreply.github.com>
2023-07-16 02:17:13 +0300

    2.3_circle_area

---
+++ 2.3_circle_area/2.3_circle_area.js
@@ -0,0 +1,5 @@
+function CircleArea(radius){
+    let area = Math.paw(radius, 2) * Math.PI;
+    console.log("The area of the circle is: " + area);
+    console.log("Rounded to two decimal places: " + area.toFixed(2)); 
+}
