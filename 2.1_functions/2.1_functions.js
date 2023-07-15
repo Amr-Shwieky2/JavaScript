59fb9f877748736d70aedfc428d895983162898b
Amr-Shwieky2 <131922418+Amr-Shwieky2@users.noreply.github.com>
2023-07-16 01:45:20 +0300

    2.1_functions

---
+++ 2.1_functions/2.1_functions.js
@@ -0,0 +1,7 @@
+function countryInfo(country, population, capitalCity){
+    return country +" has "+ population +
+     " and its capital city is " + capitalCity;
+}
+console.log(countryInfo("China", "1.4 billion people", "Beijing"));
+console.log(countryInfo("Brazil", "213 million people", "Brasília"));
+console.log(countryInfo("Germany", "83 million people", " Berlin"));
