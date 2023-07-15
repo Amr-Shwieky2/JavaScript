6a3464d0bc7ed438612235107e811471a2dd4081
Amr-Shwieky2 <131922418+Amr-Shwieky2@users.noreply.github.com>
2023-07-16 01:56:21 +0300

    2.2_functions

---
+++ 2.2_functions/2.2_functions.js
@@ -0,0 +1,9 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren) {
+    return "You will be a " + jobTitle + " in " + location + " married to " + partnerName + " with " + numberOfChildren + " children.";
+  }
+  console.log(tellFortune("programmer", "New York", "Rana", 2));
+  
+  console.log(tellFortune("teacher", "London", "John", 1));
+ 
+  console.log(tellFortune("chef", "Paris", "Maria", 0));
+  
